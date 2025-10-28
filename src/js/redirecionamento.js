592b9a579e1596633838b13c1ffb8b775658b11a
MalklaM <jabsaqua@hotmail.com>
2025-10-28 20:49:57 -0300

    login -> menu

--- src/js/redirecionamento.js
+++ src/js/redirecionamento.js
@@ -16,5 +16,19 @@ cardVisualizar.addEventListener("click", () => {
   window.location.href = "visualizar_produto.html";
 });
 
+function fazerLogin() {
+  // Captura os valores digitados
+  const usuario = document.getElementById('usuario').value;
+  const senha = document.getElementById('senha').value;
+
+  // Aqui você pode validar o login (exemplo simples)
+  if (usuario === "vendedor" && senha === "1234") {
+    // Redireciona para o menu do vendedor
+    window.location.href = "../screens/menu_vendedor.html";
+  } else {
+    // Mostra alerta se usuário ou senha estiverem incorretos
+    alert("Usuário ou senha incorretos!");
+  }
+}
 
 
