be6e2e9e537c8bbd520a074ebe88ab9db28fcbfc
bruno <bpires.siqueira@gmail.com>
2025-10-28 22:18:10 -0300

    feat: funcionalidade login

--- src/js/login.js
+++ src/js/login.js
@@ -1,14 +1,17 @@
 function fazerLogin() {
-  // Captura os valores digitados
   const usuario = document.getElementById("usuario").value;
   const senha = document.getElementById("senha").value;
 
-  // Aqui você pode validar o login (exemplo simples)
   if (usuario === "vendedor" && senha === "1234") {
-    // Redireciona para o menu do vendedor
+    localStorage.setItem("usuarioLogado", usuario);
+
     window.location.href = "../../assets/screens/menu_vendedor.html";
   } else {
-    // Mostra alerta se usuário ou senha estiverem incorretos
     alert("Usuário ou senha incorretos!");
   }
+}
+
+function sair_da_conta() {
+  window.location.href = "../../assets/screens/login.html";
+  localStorage.removeItem("usuarioLogado");
 }
