c21f44168981f421e4946c3f6e674f939a47f325
bruno <bpires.siqueira@gmail.com>
2025-10-30 11:54:10 -0300

    fix: visualizar produtos

--- src/js/visualizar_produto.js
+++ src/js/visualizar_produto.js
@@ -11,7 +11,6 @@ export async function getProdutos() {
   );
 
   if (!produtoContainer) return;
-
   let html = "";
   produtos.forEach((produto) => {
     html += `
@@ -27,11 +26,42 @@ export async function getProdutos() {
         </div>
         <div class="botoes-container">
           <button class="btn btn-editar">Editar</button>
-          <button class="btn btn-excluir">Excluir</button>
+          <button class="btn btn-excluir" data-id="${produto.id}">Excluir</button>
         </div>
       </div>
     `;
   });
   produtoContainer.innerHTML = html;
+  const botoesExcluir = document.querySelectorAll(".btn-excluir");
+  botoesExcluir.forEach((btn) => {
+    btn.addEventListener("click", deletarProduto);
+  });
+
+  const botoesEditar = document.querySelectorAll(".btn-editar");
+  botoesEditar.forEach((btn) => {
+    btn.addEventListener("click", editarProduto);
+  });
 }
+
 getProdutos();
+
+function deletarProduto(event) {
+  const id = event.currentTarget.dataset.id;
+  event.preventDefault();
+  const objProduto = { id: id };
+  if (confirm("TEM CERTEZA QUE DESE JA EXCLUIR O PRODUTO SELECIONADO?")) {
+    alert("PRODUTO EXCLUÍDO COM SUCESSO!");
+    fetch(`${url}/${objProduto.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(objProduto),
+    }).then(() => window.location.reload());
+  }
+}
+
+function editarProduto(event) {
+  const id = event.currentTarget.dataset.id;
+  window.location.href = `editar_produto.html?id=${id}`;
+}
